d9dd277628fbb912119fdd2f08aac8402d6c7a22
henryjameslau <henry.james.lau@gmail.com>
2023-01-25 10:26:03 +0000

    Let's give it a go

---
+++ postprocess.py
@@ -0,0 +1,60 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+import pandas as pd
+
+sparql = SPARQLWrapper("https://landregistry.data.gov.uk/landregistry/query")
+sparql.setReturnFormat(JSON)
+
+sparql.setQuery("""
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix owl: <http://www.w3.org/2002/07/owl#>
+prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+prefix sr: <http://data.ordnancesurvey.co.uk/ontology/spatialrelations/>
+prefix ukhpi: <http://landregistry.data.gov.uk/def/ukhpi/>
+prefix lrppi: <http://landregistry.data.gov.uk/def/ppi/>
+prefix skos: <http://www.w3.org/2004/02/skos/core#>
+prefix lrcommon: <http://landregistry.data.gov.uk/def/common/>
+
+# House price index for all regions within a given date range
+SELECT ?regionName ?code ?date ?hpi ?hpiDetached ?hpiFlatMaisonette ?hpiSemiDetached ?hpiTerraced ?averagePriceDetached ?averagePriceFlatMaisonette ?averagePriceSemiDetached ?averagePriceTerraced
+{
+  ?region ukhpi:refPeriodStart ?date;
+          ukhpi:housePriceIndex ?hpi;
+          ukhpi:housePriceIndexDetached ?hpiDetached;
+          ukhpi:housePriceIndexFlatMaisonette ?hpiFlatMaisonette;
+          ukhpi:housePriceIndexSemiDetached ?hpiSemiDetached;
+          ukhpi:housePriceIndexTerraced ?hpiTerraced;
+          ukhpi:averagePriceDetached ?averagePriceDetached;
+          ukhpi:averagePriceFlatMaisonette ?averagePriceFlatMaisonette;
+          ukhpi:averagePriceSemiDetached ?averagePriceSemiDetached;
+          ukhpi:averagePriceTerraced ?averagePriceTerraced.
+
+  ?region ukhpi:refRegion ?regionRef.
+  
+#  ?regionRef owl:sameAs ?code.
+  ?regionRef rdfs:seeAlso ?code.
+             
+  ?regionRef rdfs:label ?regionName.
+  
+
+  FILTER (langMatches( lang(?regionName), "EN")&&
+         ?date > "2015-12-31"^^xsd:date)
+             
+  FILTER contains(str(?code),"gov")
+}
+    """
+)
+
+try:
+    ret = sparql.queryAndConvert()
+
+#     for r in ret["results"]["bindings"]:
+#         print(r)
+except Exception as e:
+    print(e)
+
+df=pd.json_normalize(ret["results"]["bindings"])
+df['code']=df['code.value'].str.split('/',expand=True)[5]
+
+df2=df[['regionName.value', 'code','date.value','hpi.value','hpiDetached.value','hpiFlatMaisonette.value','hpiSemiDetached.value','hpiTerraced.value', 'averagePriceDetached.value','averagePriceFlatMaisonette.value','averagePriceSemiDetached.value','averagePriceTerraced.value' ]]
+df2.to_csv('landreg.csv',index=False)
